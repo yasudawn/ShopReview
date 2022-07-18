4a093e7b71e386ba069e4dcb4650df962b7c6626
yasudawn <yasuyuki.takamatsu@garage-ventures.com>
2022-07-19 08:38:41 +0900

    ログイン時に遷移しない不具合を修正

--- app/src/screens/UserScreen.tsx
+++ app/src/screens/UserScreen.tsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
-import { StyleSheet, SafeAreaView, Platform ,StatusBar ,Text } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { StyleSheet, SafeAreaView, Platform ,StatusBar } from "react-native";
 /* types */
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "../types/navigation";
 import { RouteProp } from "@react-navigation/native";
+/* components */
+import { Form } from "../components/Form";
+import { Button } from "../components/Button";
+/* contexts */
+import { UserContext } from "../contexts/userContext";
 
 // Style
 const styles = StyleSheet.create({
@@ -22,11 +27,28 @@ type Props = {
 
 // Main
 export const UserScreen: React.FC<Props> = ({ navigation, route }: Props) => {
+    const { user, setUser } = useContext(UserContext);
+    // 名前を保存するためのステート
+    const [name, setName] = useState<string>(user.name);
+
     useEffect(() => {});
+
+    const onSubmit = async () => {
+        // setLoading(true);
+        // const updatedAt = firebase.firestore.Timestamp.now();
+        // await updateUser(user.id, { name, updatedAt });
+        // setUser({ ...user, name, updatedAt });
+        // setLoading(false);
+    };
 
     return (
         <SafeAreaView style={styles.container}>
-                <Text>UserScreen</Text>
+            <Form
+             value={name}
+             onChangeText={(text) => {
+                setName(text);
+             }} label="名前" />
+             <Button onPress={onSubmit} text="保存する" />
         </SafeAreaView>
     );
 };
